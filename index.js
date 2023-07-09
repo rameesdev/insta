5893013f1a38c600ff0b022f32dc5c71e0a59079
rameesdev <muhammadramees697@gmail.com>
2023-07-10 00:44:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { IgApiClient } = require('instagram-private-api');
+const { readFile } = require('fs').promises;
+const cron = require("node-cron")
+require("dotenv").config();
+var day = 1;
+async function postToInstagram() {
+  const ig = new IgApiClient();
+  const username = process.env.IG_USERNAME;
+  const password = process.env.IG_PASSWORD;
+
+  // Initialize the client
+  ig.state.generateDevice(username);
+  await ig.account.login(username, password);
+ 
+  console.log('Logged in successfully!');
+
+  const photoBuffer = await readFile('rizz.mp4');
+  const photo = await readFile("download.jpg");
+  console.log(photoBuffer.byteLength)
+ 
+
+  const upload = await ig.publish.video({
+    video: photoBuffer,
+    caption: `Day ${day}`,
+    coverImage:photo
+  });
+
+  
+}
+
+
+
+cron.schedule('30 7 * * *', () => {
+  day++;
+ postToInstagram()
+},{
+  scheduled: true,
+  timezone:"Asia/Kolkata"
+});
+
